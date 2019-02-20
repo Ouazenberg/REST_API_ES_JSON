6a52acff0446e00aa7c7d840f0b4b0e0f189b5f2
Ouazenberg <33060971+Ouazenberg@users.noreply.github.com>
2019-02-20 15:24:11 +0100

    Update UsersResource.java

--- standalone-elastic-example/src/main/java/com/teckprimers/elastic/standaloneelasticexample/UsersResource.java
+++ standalone-elastic-example/src/main/java/com/teckprimers/elastic/standaloneelasticexample/UsersResource.java
@@ -1,8 +1,9 @@
 package com.teckprimers.elastic.standaloneelasticexample;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import org.apache.commons.io.FileUtils;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -10,13 +11,15 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import entities.CarFilter;
 
@@ -28,32 +31,44 @@ public class UsersResource {
 	@GetMapping("/get" )
 	public Object get() throws IOException{
 		
-		//String jsonString = "{\"vehicle_color\":\"Blanc Alfa\",\"vehicle_transmission_type_code\":\"MECA\"}";
-		
-		 JSONParser parser = new JSONParser();
-		 
-		 try {
-			 
-	            Object obj = parser.parse(new FileReader("/home/feoua/Téléchargements/filter.json"));
-	            JSONObject jsonObject = (JSONObject) obj;
-	            String jsonString = jsonObject.toString();
+		 try {	 	
+			 	//read filtering elements from filter.json file 
+	            File jsonFile = new File("/home/feoua/Téléchargements/filter.json");	    		 
+	    		String jsonString = FileUtils.readFileToString(jsonFile);
 	            
+	            //Print the jsonString 
 	            System.out.println(jsonString);
 	            
+	            //Put the recovered elements into a java object carFilter
 	            ObjectMapper mapper = new ObjectMapper();
 	    		CarFilter carFilter = mapper.readValue(jsonString, CarFilter.class);
 	    		
-	    		
+	    		//Create the TransportClient to connect with the index agil_auto in ElasticSearch 
 	    		TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
 	    		        .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
 	    		
+	    		//Set the query to recover the needed data depending on the filter elements
 	    		BoolQueryBuilder first = QueryBuilders.boolQuery()
 	    				.must(QueryBuilders.matchQuery("vehicle_color", carFilter.getVehicle_color()))
 	    				.must(QueryBuilders.matchQuery("vehicle_transmission_type_code", carFilter.getVehicle_transmission_type_code()));
-
 	    		
-	            SearchResponse searchResponse = client.prepareSearch("agil_auto").setQuery(first).get();
-	            
+	    		//Execute the query and get the response of elasticSearch 
+	    		SearchResponse searchResponse = client.prepareSearch("agil_auto").setQuery(first).get();
+	            	    		
+	    		//Convert the jsonString to a jsonObject 
+	    		JsonElement jelement = new JsonParser().parse(jsonString);
+	    		JsonObject jobject = jelement.getAsJsonObject();
+	    		
+	    		//Set the changes for the update
+	    		jobject.addProperty("vehicle_color", "Gris Acier");
+	    		jobject.addProperty("vehicle_transmission_type_code", "Auto");
+	    		
+	    		//Write and update the new changes into the jsonFile
+	    		Gson gson = new Gson();
+	    		String resultingJson = gson.toJson(jelement);
+	    		FileUtils.writeStringToFile(jsonFile, resultingJson);
+	    		 
+	    		//Return the ES search results
 	        	return searchResponse;
 	 
 	        } catch (Exception e) {
